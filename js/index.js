8ce22bad527962c64ae3786f5f722a9393b1de0e
davedwight <dpdwight@gmail.com>
2021-05-04 13:55:45 -0600

    Added first eventListener for mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+// * `mouseover`
+// * `keydown`
+// * `wheel`
+// * `load`
+// * `focus`
+// * `resize`
+// * `scroll`
+// * `select`
+// * `dblclick`
+// * `drag / drop`
+
+const header = document.querySelector('header');
+const logo = document.querySelector('h1.logo-heading');
+
+logo.addEventListener('mouseover', function(event){
+    header.style.backgroundColor = 'red';
+})
